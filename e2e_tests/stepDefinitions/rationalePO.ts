d9d7cac4722c473b20d912b2a5e3b9e97b0efebb
mayank-s18 <mayank.solanki@digital.justice.gov.uk>
2024-03-20 17:24:21 +0000

    mrd-2391 handle additional question in task list

--- e2e_tests/stepDefinitions/rationalePO.ts
+++ e2e_tests/stepDefinitions/rationalePO.ts
@@ -455,14 +455,14 @@ const createPartAOrNoRecallLetter = function (partADetails?: Record<string, stri
     })
     cy.clickButton('Continue')
   }
+  cy.clickLink(`Local police contact details`)
+  cy.logPageTitle('Local police contact details')
+  testData.localPoliceDetails = {}
+  cy.fillInput('Police contact name', (testData.localPoliceDetails.contact = faker.name.fullName()))
+  cy.fillInput('Telephone number', (testData.localPoliceDetails.phoneNumber = '01277 960 001'))
+  cy.fillInput('Email address', (testData.localPoliceDetails.email = faker.internet.email()))
+  cy.clickButton('Continue')
   if (!['YES_POLICE', 'YES_PRISON'].includes(testData.inCustody)) {
-    cy.clickLink(`Local police contact details`)
-    cy.logPageTitle('Local police contact details')
-    testData.localPoliceDetails = {}
-    cy.fillInput('Police contact name', (testData.localPoliceDetails.contact = faker.name.fullName()))
-    cy.fillInput('Telephone number', (testData.localPoliceDetails.phoneNumber = '01277 960 001'))
-    cy.fillInput('Email address', (testData.localPoliceDetails.email = faker.internet.email()))
-    cy.clickButton('Continue')
     currentPage = `Is there anything the police should know before they arrest ${this.offenderName}`
     cy.clickLink(currentPage)
     cy.logPageTitle(`${currentPage}?`)
