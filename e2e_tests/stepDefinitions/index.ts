f25794473c0892b2f002ccdda1d4dd558137b5e3
mayank-s18 <mayank.solanki@digital.justice.gov.uk>
2024-04-25 09:34:25 +0100

    MRD-2483 cleanup unused flags

--- e2e_tests/stepDefinitions/index.ts
+++ e2e_tests/stepDefinitions/index.ts
@@ -86,9 +86,6 @@ function loginAndSearchCrn(userType: UserType) {
     {
       flagRecommendationsPage: 1,
       flagDeleteRecommendation: 1
-      // flagTriggerWork: 1,
-      // flagLastCompleted: 1,
-      // flagPpcs:1
     },
     userType
   )
