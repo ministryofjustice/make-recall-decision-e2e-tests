162d018d8a6acb9ebac5fa94f6331c622467cfc9
Pablo Glenn <pablo.glenn@digital.justice.gov.uk>
2025-12-09 10:55:28 +0000

    MRD-3007 Add npm-script-allowlist to project

---
+++ .allowed-scripts.mjs
@@ -0,0 +1,11 @@
+import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-allowlist'
+
+export default configureAllowedScripts({
+   allowlist: {
+     "node_modules/@badeball/cypress-cucumber-preprocessor@22.2.0": "ALLOW",
+     "node_modules/cypress@13.17.0": "ALLOW",
+     "node_modules/dtrace-provider@0.8.8": "ALLOW",
+     "node_modules/esbuild@0.25.9": "ALLOW",
+     "node_modules/fsevents@2.3.3": "ALLOW"
+   },
+})
