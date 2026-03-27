e976e1807a41a770f4f8cf0bbe9ec8ea609cc810
Suresh Mandalapu <231466518+sureshmoj@users.noreply.github.com>
2026-03-27 16:08:50 +0000

    MRD-3095: new enum value for IndeterminateOrExtendedSentenceDetailType

--- e2e_tests/stepDefinitions/ppcs/ppcs.ts
+++ e2e_tests/stepDefinitions/ppcs/ppcs.ts
@@ -52,6 +52,7 @@ export const recallTypes: OptionValue[] = [
 export const indeterminateOrExtendedSentenceDetails: OptionValue[] = [
     { text: "{{ fullName }} has shown behaviour similar to the index offence", value: "BEHAVIOUR_SIMILAR_TO_INDEX_OFFENCE" },
     { text: "{{ fullName }} has shown behaviour that could lead to a sexual or violent offence", value: "BEHAVIOUR_LEADING_TO_SEXUAL_OR_VIOLENT_OFFENCE" },
+    { text: "{{ fullName }} has shown behaviour likely to result in a sexual or violent offence, or that could be associated with committing one", value: "BEHAVIOUR_LIKELY_TO_RESULT_SEXUAL_OR_VIOLENT_OFFENCE" },
     { text: "{{ fullName }} is out of touch", value: "OUT_OF_TOUCH" }
 ]
 
