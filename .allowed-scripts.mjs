3786388213cd7e89091019b5e97cb3898ef86ed5
fincardy-moj <fin.cardy@justice.gov.uk>
2026-04-20 14:04:28 +0100

    MRD-3177 upgrade cypress and dependencies

--- .allowed-scripts.mjs
+++ .allowed-scripts.mjs
@@ -2,9 +2,8 @@ import { configureAllowedScripts } from '@ministryofjustice/hmpps-npm-script-all
 
 export default configureAllowedScripts({
    allowlist: {
-     "node_modules/@badeball/cypress-cucumber-preprocessor@22.2.0": "ALLOW",
      "node_modules/@parcel/watcher@2.5.6": "ALLOW",
-     "node_modules/cypress@13.17.0": "ALLOW",
+     "node_modules/cypress@15.14.0": "ALLOW",
      "node_modules/dtrace-provider@0.8.8": "ALLOW",
      "node_modules/esbuild@0.27.7": "ALLOW",
      "node_modules/fsevents@2.3.3": "ALLOW"
