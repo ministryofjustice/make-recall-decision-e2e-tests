f328baab120b25271092bdee4a3725137d33d9af
Bhanu Yandra <bhanu.yandra@justice.gov.uk>
2026-09-22 16:32:21 +0100

    MRD-3375: new standard licence conditions

--- e2e_tests/support/enums.ts
+++ e2e_tests/support/enums.ts
@@ -85,6 +85,17 @@ export enum LicenceConditions {
   CONTACT_DETAILS = 'Tell your supervising officer if you change or add any contact details, including phone number or email',
 }
 
+export enum NewLicenceConditions {
+  GOOD_BEHAVIOUR = 'Behave well in a way that supports the purpose of you being on licence, and do not commit any crime.',
+  KEEP_IN_TOUCH = 'Keep in touch and meet with your supervising officer in the way they tell you to. This includes meeting them where you live.',
+  ADDRESS_APPROVED = 'Get permission from your supervising officer to stay at an address and if you want to stay somewhere else for one or more nights.',
+  NO_WORK_UNDERTAKEN = 'Tell your supervising officer about any new work, or a type of work, you want to do. Get their approval before you start this work',
+  NO_TRAVEL_OUTSIDE_UK = 'Get permission from your supervising officer if you want to leave the United Kingdom, Isle of Man or the Channel Islands. This does not apply if you are being deported or removed for immigration purposes.',
+  NAME_CHANGE = 'Tell your supervising officer about any names you use that are different to the names on this licence.',
+  CONTACT_DETAILS = 'Inform your supervising officer if your contact details change. For example, your phone number or email address.',
+  PASSPORT_DETAILS = 'Get permission from your supervising officer if you want to apply for a new passport. If requested, tell your supervising officer about any passports you have already.',
+}
+
 export enum WhyConsiderRecall {
   INCREASED_RIS = 'Your risk is assessed as increased',
   CONTACT_WITH_PO_BROKEN = 'Contact with your probation practitioner has broken down',
